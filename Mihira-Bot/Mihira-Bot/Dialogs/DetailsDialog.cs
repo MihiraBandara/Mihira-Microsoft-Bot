1ff0fe382001e9c9b1be8a83395c5398598c0ccc
Mihira <mihira2u@gmail.com>
2018-04-02 08:38:22 +0530

    added all files in bot project

---
+++ Mihira-Bot/Mihira-Bot/Dialogs/DetailsDialog.cs
@@ -0,0 +1,44 @@
+﻿using Bot_Application1.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bot_Application1.Dialogs
+{
+    public class DetailsDialog
+    {
+        public static readonly IDialog<string> dialog = Chain.PostToChain()
+            .Select(msg => msg.Text)
+            .Switch(
+                 new RegexCase<IDialog<string>>(new Regex("^hi", RegexOptions.IgnoreCase), (context, txt) =>
+                 {
+                     return Chain.ContinueWith(new GreetingDialog(), AfterGreetingContinuation);
+                 }),
+                 new DefaultCase<string, IDialog<string>>((context, txt) =>
+                 {
+                     return Chain.ContinueWith(FormDialog.FromForm(DetailsForm.BuildForm, FormOptions.PromptInStart), AfterDetailsContinuation);
+                 }))
+             .Unwrap()
+             .PostToUser();
+
+        private async static Task<IDialog<string>> AfterGreetingContinuation(IBotContext context, IAwaitable<object> res)
+        {
+            var token = await res;
+            var name = "User";
+            context.UserData.TryGetValue<string>("Name", out name);
+            return Chain.Return($"Please enter Yes if you want to continue : {name}");
+        }
+        private async static Task<IDialog<string>> AfterDetailsContinuation(IBotContext context, IAwaitable<object> res)
+        {
+            var token = await res;
+            var name = "User";
+            context.UserData.TryGetValue<string>("Name", out name);
+            return Chain.Return($"Thank you for using the contact details bot: {name}");
+        }
+    }
+}
---
+++ Mihira-Bot/Mihira-Bot/Models/DetailsForm.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application1.Models
+{
+    public enum GenderOptions
+    {
+        Male,
+        Female
+    }
+
+    public enum ProvincialOptions
+    {
+        CentralProv,
+        Western,
+        NorthCent,
+        Sabaragamuwa,
+        Uva,
+        NorthWest,
+        South,
+        North,
+        East
+    }
+
+    [Serializable]
+    public class DetailsForm
+    {
+        public String FirstName;
+        public String LastName;
+        public GenderOptions? Gender;
+        public int? PostalCode;
+        public DateTime? DateOfBirth;
+        public int? Telephone;
+        public ProvincialOptions? Province;
+
+        public static IForm<DetailsForm> BuildForm()
+        {
+            return new FormBuilder<DetailsForm>()
+                .Message("Welcome to the contact details bot!")
+                .Confirm("Do you want to add your Details?")
+                .Build();
+        }
+
+    }
+}
